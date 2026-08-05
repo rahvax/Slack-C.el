f5af4832b3b5525e4935b0785a2989728a86157d
rahvax <gabriel@caelestis.one>
2026-08-05 17:43:00 -0300

    Fix nome

--- SlackC.el
+++ SlackC.el
@@ -1,4 +1,4 @@
-;;; SlackC.el --- Criando projetos em C pré-configurados -*- lexical-binding: t; -*-
+;;; slackc.el --- Criando projetos em C pré-configurados -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2026 Gabriel Caelestis
 
